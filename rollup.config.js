c939fc77704adc1db091c78338391b84e39a09b7
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2019-10-16 09:18:54 +0200

    Fixed yarn audit

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,43 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-// import postcss from 'rollup-plugin-postcss-modules'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.tsx',
-  external: ['stream'],
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
-    svgr(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true
-    }),
-    commonjs({
-      include: 'node_modules/**',
-      namedExports: {
-        'node_modules/react-is/index.js': ['isElement','isValidElementType','ForwardRef']
-      }
-    })
-  ]
-}
+    input: "src/index.tsx",
+    external: ["stream"],
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            exports: "named",
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: "es",
+            exports: "named",
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        external(),
+        url(),
+        svgr(),
+        resolve(),
+        typescript({
+            rollupCommonJSResolveHack: true,
+            clean: true,
+        }),
+        commonjs({
+            include: "node_modules/**",
+            namedExports: {
+                "node_modules/react-is/index.js": ["isElement", "isValidElementType", "ForwardRef"],
+            },
+        }),
+    ],
+};
